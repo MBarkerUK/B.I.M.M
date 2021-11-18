22d5ecc5ea35960fb53676ce3b61b9e7e444cee5
Matt <94524444+lErrorl404l@users.noreply.github.com>
2021-11-18 09:09:10 +0000

    Add files via upload

--- BIMM-Linux.sh
+++ BIMM-Linux.sh
@@ -1,5 +1,6 @@
-read -p "Name of the modlist file: " filename
-clear
+#!/bin/bash
+
+filename="$(zenity --file-selection --file-filter='HTML files (html) | *.html' --title"Select your mod preset")"
 
 grep -a '"DisplayName"' $filename | cut -d ">" -f2 | cut -d "<" -f1 | tr " " _ | sed 's/^/@/' | tr '\n' ';' > ModList.txt
 printf "Your File Is Now Ready To Be Copy And Pasted Into A Config\n"
