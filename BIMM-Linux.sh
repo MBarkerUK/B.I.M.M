1433012080e25b7bdb36397bb6ca10581842cbdb
Matt <94524444+lErrorl404l@users.noreply.github.com>
2021-11-20 18:38:04 +0000

    Add files via upload

--- BIMM-Linux.sh
+++ BIMM-Linux.sh
@@ -2,6 +2,6 @@
 
 filename="$(zenity --file-selection --file-filter='HTML files (html) | *.html' --title"Select your mod preset")"
 
-grep -a '"DisplayName"' $filename | cut -d ">" -f2 | cut -d "<" -f1 | tr " " _ | sed 's/^/@/' | tr '\n' ';' > ModList.txt
+grep -a '"DisplayName"' $filename | cut -d ">" -f2 | cut -d "<" -f1 | sed 's/^/@/' | tr '\n' ';' > ModList.txt
 printf "Your File Is Now Ready To Be Copy And Pasted Into A Config\n"
 printf "Default File Name: ModList.txt"
--- BIMM-Windows.ps1
+++ BIMM-Windows.ps1
@@ -8,7 +8,6 @@ $Global:SelectedFile = $OpenFileDialog.FileName
 
 
 Get-Content "$SelectedFile" | findstr "DisplayName" | ForEach-Object { ([string]$_).Split(">")[1] } | ForEach-Object { ([string]$_).Split("<")[0] } > ModList.txt 
-((Get-Content -Path ModList.txt) -replace ' ','_') | Set-Content -Path ModList.txt
 (Get-Content -Path ModList.txt) -join ";@" | Set-Content -Path ModList.txt
 Write-Host "Your File Is Now Ready To Be Copy And Pasted Into A Config"
 Write-Host "Default File Name: ModList.txt"
