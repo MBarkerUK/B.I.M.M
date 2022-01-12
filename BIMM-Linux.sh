b57ce2c52f23b95f05b13d407cc45e9508b8a3ac
lErrorl404l <lErrorl404l@gmail.com>
2022-01-12 19:27:31 +0000

    Added in some comments, nothing major

--- BIMM-Linux.sh
+++ BIMM-Linux.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+#Open file selection window to select preset
 filename="$(zenity --file-selection --file-filter='HTML files (html) | *.html' --title"Select your mod preset")"
 
+#Take selected file and cut away everything apart from the mod names
 grep -a '"DisplayName"' $filename | cut -d ">" -f2 | cut -d "<" -f1 | sed 's/^/@/' | tr '\n' ';' > ModList.txt
 printf "Your File Is Now Ready To Be Copy And Pasted Into A Config\n"
 printf "Default File Name: ModList.txt"
--- BIMM-Windows.ps1
+++ BIMM-Windows.ps1
@@ -1,13 +1,16 @@
+#Open file selection window to select preset
 Add-Type -AssemblyName System.Windows.Forms
 $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
 $OpenFileDialog.Title = "Please Select File"
 $OpenFileDialog.InitialDirectory = $initialDirectory
 $OpenFileDialog.filter = "Preset files (*.html)|*.html"
 $OpenFileDialog.ShowDialog() | Out-Null
+#Using selected file as input for the rest of the code
 $Global:SelectedFile = $OpenFileDialog.FileName
 
-
-Get-Content "$SelectedFile" | findstr "DisplayName" | ForEach-Object { ([string]$_).Split(">")[1] } | ForEach-Object { ([string]$_).Split("<")[0] } > ModList.txt 
+#Take selected file and cut away everything apart from the mod names
+Get-Content "$SelectedFile" | findstr "DisplayName" | ForEach-Object { ([string]$_).Split(">")[1] } | ForEach-Object { ([string]$_).Split("<")[0] } > ModList.txt
+#Connect mod names with ;@ in between, formatting them as required by BI
 (Get-Content -Path ModList.txt) -join ";@" | Set-Content -Path ModList.txt
 Write-Host "Your File Is Now Ready To Be Copy And Pasted Into A Config"
 Write-Host "Default File Name: ModList.txt"
